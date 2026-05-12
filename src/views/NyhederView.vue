cb8aa577fcc81a56ee47b6766f5337542d88ae72
Mogensan <Andmog01@easv365.dk>
2026-05-12 10:21:17 +0200

    [Add] tilføjet nyheder page
    Tilføjet nyheder så vi nu har alle main pages

---
+++ src/views/NyhederView.vue
@@ -0,0 +1,27 @@
+<template>
+  <main>
+    <h1>Nyheder</h1>
+    <p>Seneste nyheder vil blive vist her.</p>
+  </main>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+main {
+  padding: 2rem;
+}
+
+h1 {
+  font-size: 2.5rem;
+  font-weight: 300;
+  color: #0D1B2A;
+  margin-bottom: 1rem;
+}
+
+p {
+  color: #415A77;
+  font-size: 1rem;
+}
+</style>
