e2ed2d3162bb9712c7bc5e9282a08492c106d1f4
Oliver Reffsøe Brik <olibri01@easv365.dk>
2026-05-14 09:17:46 +0200

    [fix] put employees into their on data folder

---
+++ src/data/employees.js
@@ -0,0 +1,38 @@
+export const employees = [
+  {
+    name: 'Dustin ettermann',
+    role: 'Rolle',
+    description: 'netudvikler',
+    phone: '+45 22 17 79 60',
+    email: 'kontakt@businessregion.dk',
+    image:
+      'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?auto=format&fit=crop&w=900&q=80',
+  },
+  {
+    name: 'Dustin ettermann',
+    role: 'Rolle',
+    description: 'arbejdsmarked',
+    phone: '+45 22 17 79 61',
+    email: 'kontakt@businessregion.dk',
+    image:
+      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?auto=format&fit=crop&w=900&q=80',
+  },
+  {
+    name: 'Dustin ettermann',
+    role: 'Rolle',
+    description: 'lokal udvikling',
+    phone: '+45 22 17 79 62',
+    email: 'kontakt@businessregion.dk',
+    image:
+      'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?auto=format&fit=crop&w=900&q=80',
+  },
+  {
+    name: 'Dustin ettermann',
+    role: 'Rolle',
+    description: 'lokal udvikling',
+    phone: '+45 22 17 79 63',
+    email: 'kontakt@businessregion.dk',
+    image:
+      'https://images.unsplash.com/photo-1488426862026-3ee34a7d66df?auto=format&fit=crop&w=900&q=80',
+  },
+]
